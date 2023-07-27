4fc33b5b46d1b9a6f8e2cce0a586c4e6f98ead09
beliberda <anton.nurgaliev@mail.ru>
2023-07-27 14:16:06 +1000

    control button

--- assets/js/audiorain.js
+++ assets/js/audiorain.js
@@ -1,4 +1,18 @@
 let audio = document.querySelector('audio')
-
+let stop = document.querySelector('.button-start')
 // Изначально меняем значение громкости
 audio.volume = 0.6
+
+let isPlay = true
+
+stop.addEventListener('click',()=>{
+    if (isPlay) {
+        audio.pause()
+        console.log('Остановили');
+        isPlay = false
+    } else {
+        audio.play()
+        console.log('Запустили');
+        isPlay = true
+    }
+})
