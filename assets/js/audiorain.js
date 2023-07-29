944f5042f92b69d158c6a86752bc47246ef2217b
beliberda <anton.nurgaliev@mail.ru>
2023-07-29 17:37:47 +1000

    fix

--- assets/js/audiorain.js
+++ assets/js/audiorain.js
@@ -2,7 +2,6 @@ let audio = document.querySelector('audio')
 let stop = document.querySelector('.button-start')
 // Изначально меняем значение громкости
 audio.volume = 0.6
-
 let isPlay = true
 
 stop.addEventListener('click',()=>{
