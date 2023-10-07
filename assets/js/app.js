f8c7b7ddcde62731dedb49feea6b62aef180ee53
beliberda <anton.nurgaliev@mail.ru>
2023-10-07 12:49:09 +1000

    desktop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,11 @@
 // Привязка к мышке
 
-document.addEventListener('mousemove', e => {
-    Object.assign(document.documentElement, {
-        style:
-        `
-        --move-x: ${(e.clientX - window.innerWidth/2)*-0.005}deg;
-        --move-y: ${(e.clientY - window.innerHeight/2)*-0.01}deg;
+document.addEventListener("mousemove", (e) => {
+  Object.assign(document.documentElement, {
+    style: `
+        --move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
+        --move-y: ${(e.clientY - window.innerHeight / 2) * -0.01}deg;
 
-        `
-    })
-})
+        `,
+  });
+});
--- assets/js/rain.js
+++ assets/js/rain.js
@@ -1,7 +1,9 @@
+// Получаем элемент с классом rain
 let canvas = document.getElementsByClassName('rain')[0];
+// Настраиваем высоту и ширину canvas 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
+// Получаем в переменную 2д контекст канвы
 let c = canvas.getContext('2d');
 
 function randomNum(max, min) {
@@ -16,17 +18,18 @@ function RainDrops(x, y, endy, velocity, opacity) {
 	this.velocity = velocity;
 	this.opacity = opacity;
 
-	this.draw = function() {
+	this.draw = function () {
+
 		c.beginPath();
 		c.moveTo(this.x, this.y);
 		c.lineTo(this.x, this.y - this.endy);
 		c.lineWidth = 1;
 
-		c.strokeStyle= `rgba(255, 255, 255, ${this.opacity})`
+		c.strokeStyle = `rgba(255, 255, 255, ${this.opacity})`
 		c.stroke();
 	}
 
-	this.update = function() {
+	this.update = function () {
 		let rainEnd = window.innerHeight + 100;
 		if (this.y >= rainEnd) {
 			this.y = this.endy - 100;
@@ -42,7 +45,7 @@ let rainArray = [];
 
 for (let i = 0; i < 240; i++) {
 	let rainXLocation = Math.floor(Math.random() * window.innerWidth) + 1;
-	let rainYLocation = Math.random()*-500;
+	let rainYLocation = Math.random() * -500;
 	let randomRainHeight = randomNum(10, 2);
 	let randomSpeed = randomNum(20, .2);
 	let randomOpacity = Math.random() * .55;
@@ -52,7 +55,7 @@ for (let i = 0; i < 240; i++) {
 function animateRain() {
 
 	requestAnimationFrame(animateRain);
-	c.clearRect(0,0, window.innerWidth, window.innerHeight);
+	c.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
 	for (let i = 0; i < rainArray.length; i++) {
 		rainArray[i].update();
